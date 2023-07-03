dd9544b6ed5ed6b37a5061800eb25cccf4fdefd3
Jibril Isah <jibrilisah1@gmail.com>
2023-07-03 22:10:32 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,22 @@
+#!/usr/bin/python3
+""" function that returns all integers in pascal's triangle"""
+
+
 def pascal_triangle(n):
+    """Create Pascal's triangle"""
     if n <= 0:
         return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
-
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    if n > 2:
+        base = [[1], [1, 1]]
+        for i in range(n - 2):
+            last = base[-1]
+            next = [1]
+            for i in range(len(last) - 1):
+                next.append(last[i] + last[i + 1])
+            next.append(1)
+            base.append(next)
+    return base
